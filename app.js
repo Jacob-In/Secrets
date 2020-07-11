cfe115fcb167298192d23a06d69a23fbf61bfdec
Jacob In <jacobin83@hotmail.com>
2020-07-11 10:45:37 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,82 @@
+//jshint esversion:6
+
+//Dotenv (environtment variables} need to be assigned at the top. We need to run one time only and don't need to assign contant to it.
+require('dotenv').config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+// Connection to the server
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+//Create Mongoose Schema
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+
+
+//It is important that we put this userSchema.plugin before the mongoose.model
+//process.env.SECRET is to grab the SECRET key inside .env file
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+//Create Mongoose Model
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", function(req, res) {
+  const newUser = new User ({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err) {
+    if (err) {
+    console.log(err)
+    } else {
+      res.render("secrets");
+      }
+  })
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password)
+          res.render("secrets");
+      }
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
